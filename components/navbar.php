8ae013fd34cb3e608b86679033ce8fbff88aa3ac
samantha <sschneibel@gaming.tech>
2022-12-27 17:27:17 +0100

    maj

---
+++ components/navbar.php
@@ -0,0 +1,15 @@
+<nav id="navbar" class="">
+
+    <div class="nav-wrapper">
+      <a href="index.php" class="brand-logo center">Portfolio</a>
+      <ul>
+        <li class="left"><a href="projects.php">Mes projets</a></li>
+        <li class="right"><a class="button modal-trigger" href="#modal1">Se connecter</a></li>
+      </ul>
+    </div>
+
+    <div id="modal1" class="modal container">
+        <h2>Connectez-vous à votre compte</h2>
+    </div>
+
+  </nav>
---
+++ js/script.js
@@ -0,0 +1,13 @@
+//for parallax and cards
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.modal').modal();
+  });
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+//wow js initialisation
+new WOW().init();
